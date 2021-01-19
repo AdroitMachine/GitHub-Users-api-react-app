798ec2d802404d7110c033ae565b13da75f447fe
AdroitMachine <anujsaxenamail@gmail.com>
2021-01-19 22:17:30 +0530

    initial commit

---
+++ src/components/Repos.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GithubContext } from '../context/context';
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+const Repos = () => {
+  const { repos } = React.useContext(GithubContext)
+  console.log(repos)
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13"
+    },
+    {
+      label: "CSS",
+      value: "23"
+    },
+    {
+      label: "Javascript",
+      value: "80"
+    },
+
+  ];
+  return (
+    <section className='section'>
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={chartData} />
+      </Wrapper>
+    </section>
+  )
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  gap: 2rem;
+  @media (min-width: 800px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1200px) {
+    grid-template-columns: 2fr 3fr;
+  }
+
+  div {
+    width: 100% !important;
+  }
+  .fusioncharts-container {
+    width: 100% !important;
+  }
+  svg {
+    width: 100% !important;
+    border-radius: var(--radius) !important;
+  }
+`;
+
+export default Repos;
